92a344d820048426c41d1551079ecaa6ccbb8dae
Sung-il Yoon <books13456@gmail.com>
2022-08-24 12:33:01 +0900

    Add flatlist contains card of water image and name

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,25 +1,8 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import WaterCardContainer from '../containers/WaterCardContainer';
 
 function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
-      <Button title="Go to Details" onPress={() => {}} />
-    </View>
-  );
+  return <WaterCardContainer />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-});
 
 export default HomeScreen;
---
+++ screens/WaterCardDetailsScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native ';
+
+function WaterCardDetailsScreen() {
+  return (
+    <View>
+      <Text>WaterCardDetailsScreen</Text>
+    </View>
+  );
+}
+
+export default WaterCardDetailsScreen;
