5daa322878f1da19276b33f96944f00cbb9c6114
Sung-il Yoon <books13456@gmail.com>
2022-08-26 10:42:54 +0900

    Add FlatList shows carditems in 2 columns

--- containers/WaterCardContainer.js
+++ containers/WaterCardContainer.js
@@ -15,21 +15,31 @@ const data = [
     location: 'Location',
     image: require('../img/logo.png'),
   },
-  {
-    id: 3,
-    name: 'Water',
-    location: 'Location',
-    image: require('../img/logo.png'),
-  },
 ];
+
+function a(d) {
+  for (let i = 3; i < 20; i++) {
+    d.push({
+      id: i,
+      name: 'Water',
+      location: 'Location',
+      image: require('../img/logo.png'),
+    });
+  }
+}
+
+a(data);
 
 function WaterCardContainer() {
   return (
     <FlatList
       style={styles.block}
+      columnWrapperStyle={styles.columnWrapperStyle}
       data={data}
       renderItem={(item) => <WaterCard water={item.item} />}
       keyExtractor={(item) => item.id}
+      key={1}
+      numColumns={2}
     />
   );
 }
@@ -38,6 +48,11 @@ const styles = StyleSheet.create({
   block: {
     flex: 1,
     backgroundColor: '#F5FCFF',
+  },
+  columnWrapperStyle: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
 });
 
